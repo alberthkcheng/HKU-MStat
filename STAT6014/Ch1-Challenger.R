5af23f51b885afad1dc090004ef457af91051f39
Albert Cheng <albert.hk.cheng@gmail.com>
2016-03-28 13:56:05 +0800

    update stat6014

--- STAT6014/Ch1-Challenger.R
+++ STAT6014/Ch1-Challenger.R
@@ -1,4 +1,7 @@
 # Challenger data
+library(broom)
+library(aod)
+
 # Load data set
 challenger <- read.table("data/ch1-challenger.txt", header = TRUE)
 
@@ -6,3 +9,9 @@ challenger <- read.table("data/ch1-challenger.txt", header = TRUE)
 fit <- glm(failure ~ temp, data = challenger, family = binomial)
 summary(fit)
 exp(fit$coefficients["temp"]) # Odd ratio for 1 degree increase 84.26% -- <16% reduction in odd
+
+# Testing Global Null Hypothesis: BETA=0
+anova(fit,test = "LRT") # LRT test
+anova(fit,test = "Rao") # Score test
+wald.test(b = coef(fit), Sigma = vcov(fit), Terms = 2) # Wald test
+
--- STAT6014/Ch1-Neuralgia.R
+++ STAT6014/Ch1-Neuralgia.R
@@ -1,17 +1,39 @@
 # Neuralgia data
 # https://support.sas.com/documentation/cdl/en/statug/63347/HTML/default/viewer.htm#statug_logistic_sect060.htm
+library(broom)
+library(dplyr)
+library(tidyr)
+library(aod)
 
 neuralgia <- read.table("data/ch1-neuralgia.txt", header = TRUE)
-neuralgia$treatment <- relevel(neuralgia$treatment, ref="P")
+neuralgia$treatment <- relevel(neuralgia$treatment, ref = "P")
 
-fit <- glm(npain/n~treatment, family = binomial, data = neuralgia)
+fit <- glm(npain/n~treatment, family = binomial, data = neuralgia, weights = n)
 summary(fit)
-cbind(neuralgia,log_odd=predict(fit), odd_ratio = exp(predict(fit)), p_value = predict(fit,type="response"))
+anova(fit,test = "LRT") # LRT test
+anova(fit,test = "Rao") # Score test
+
+neuralgia <- neuralgia %>% rowwise() %>% mutate(trial = list(c(rep(1,npain),rep(0,n - npain)))) %>% unnest()
+
+fit <- glm(trial~treatment, family = binomial, data = neuralgia)
+summary(fit)
+newdata <- data.frame(treatment = c("A","B","P"))
+prediction <-  predict(fit,newdata = newdata)
+cbind(newdata,log_odd = prediction, odd_ratio = exp(prediction), p_value = predict(fit,newdata = newdata,type = "response"))
 # Odds Ratio A:B
-exp(predict(fit)[1]-predict(fit)[2])
+exp(prediction[1] - prediction[2])
 # Equivalent to 
-neuralgia$treatment <- relevel(neuralgia$treatment, ref="B")
-fit <- glm(npain/n~treatment, family = binomial, data = neuralgia)
-summary(fit)
-exp(coef(fit)["treatmentA"])
+neuralgia$treatment <- relevel(neuralgia$treatment, ref = "B")
+fit2 <- glm(trial~treatment, family = binomial, data = neuralgia)
+summary(fit2)
+exp(coef(fit2)["treatmentA"])
 # 25% reduction by treatment A compared to treatment B
+
+# Wald Confidence interval
+exp(confint.default(fit))
+
+# Testing Global Null Hypothesis: BETA=0
+anova(fit,test = "LRT") # LRT test
+anova(fit,test = "Rao") # Score test
+wald.test(b = coef(fit), Sigma = vcov(fit), Terms = 2:3) # Wald test
+
